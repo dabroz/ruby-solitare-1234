376923fca8566b55e9dbffe18d935ac0de399c55
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 21:30:57 +0200

    xx

--- go.rb
+++ go.rb
@@ -100,10 +100,11 @@ class Game
     end
     printkey(4, 1, 'q') if @mode == 'select' and (@cards.count+@grave.count) > 0
     printkey(4+11 + 4 * @select.count - 6, 1, 'w') if @mode == 'select' and @select.count > 0
+    color2(30,107)
     goto(1,HEIGHT-1)
     print " "*WIDTH*2
     goto(1,HEIGHT-1)
-    color2(30,107)
+    #color2(30,107)
     print "Mode: #{@mode}"
     if @mode == 'select'
       print " | press key to select card"
