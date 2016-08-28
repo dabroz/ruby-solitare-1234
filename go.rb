ed9f9121f7a0823930ae2178729c56ad20a97d05
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 20:06:34 +0200

    xx

--- go.rb
+++ go.rb
@@ -108,7 +108,7 @@ class Game
   end
   def color(fg,bg,bold,bgbold=false)
 #    print "\033[#{bold ? 9 : 3}#{fg};#{bgbold ? 10 : 4}#{bg}m"
-color2(bold ? 90 : 30 + fg, bgbold ? 100 : 40 + bg)
+color2((bold ? 90 : 30) + fg, (bgbold ? 100 : 40) + bg)
   end
   def color2(fg,bg)
     print "\033[#{fg};#{bg}m"
