f74d8e49dc79ccc0741ecaf6742c14cf070b9be1
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 20:40:04 +0200

    xx

--- go.rb
+++ go.rb
@@ -107,6 +107,7 @@ class Game
     end
     @target.each_with_index do |target, index|
       printcard(11+index * 11 + 37-11, 2, target[0])
+      printkey(11+index * 11 + 37-11, 2, 'a' + index)
     end
     printcard(4, 2, '')
     @select.each_with_index do |select, index|
