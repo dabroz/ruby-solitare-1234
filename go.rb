cbe6a1db21ed51f94e5daa0c957dfff9c189db84
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 18:22:46 +0200

    xx

--- go.rb
+++ go.rb
@@ -4,9 +4,20 @@ CARDS = "♠♥♦♣"
 GAMEID = ARGV[0] || rand(2**16)
 
 class Card
+  def initialize(num)
+    @num = num
+  end
+  def color
+    (num / 13).to_i
+  end
+  def value
+    num % 13 + 1
+  end
 end
 
 class Game
+  def initialize
+    @cards = (0...52).map {|n| Card.new(n) }
+    puts @cards
+  end
 end
-
-puts "game #{GAMEID}"
