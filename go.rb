78eb9e4f3543616b3fee2a8e85afdce945496834
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 18:39:07 +0200

    xx

--- go.rb
+++ go.rb
@@ -34,8 +34,19 @@ end
 class Game
   def initialize
     @cards = (0...52).map {|n| Card.new(n) }
+    @cards.shuffle(random: Random.new(GAMEID))
+    @stacks = [[],[],[],[],[],[],[]]
+    (1..7).each do |n|
+      n.times do
+        c = @cards.shift
+        @stacks[n - 1] << c
+      end
+    end
     puts @cards
+    puts @stacks
   end
 end
+
+# ╔═╗ ╚╝ ░ ▒ ▓
 
 Game.new
