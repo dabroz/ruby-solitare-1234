a43dabf624f09ea66784fdd5933b75e3bd943d01
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2016-08-28 23:05:32 +0200

    mini

--- go.rb
+++ go.rb
@@ -19,7 +19,7 @@ class Card
     color < 2
   end
   def to_s
-    "♥♦♣♠"[color] + (value!=10 ? ' ' : '') + ((0..10).to_a + %w(J Q K A))[value].to_s
+    "♥♦♣♠"[color] + (value!=10 ? ' ' : '1') + '01234567890JQKA'[value]
   end
   def revealed?
     @revealed
